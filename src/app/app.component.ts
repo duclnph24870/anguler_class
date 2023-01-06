5f80fd026544916ce22f650dae554db427726ee0
unknown <duclnph24870@fpt.edu.vn>
2023-01-06 19:51:33 +0700

    lab1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Data, Product } from './data.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'agularDemo';
+  title = 'FPT Polytecnic';
+  student={
+    name: 'Ngọc Đức',
+    gender: "Nam",
+    point: 9,
+    image: 'https://i.pinimg.com/originals/64/d5/f7/64d5f711af697325e65c5187b11290e9.jpg',
+    birthday: '13/10/2001'
+  }
+  data:Data[] = [
+    { first: 'Ngọc',last: 'Đức',year: 2001,passed: 2022 },
+    { first: 'Ngọc',last: 'Đức',year: 2001,passed: 2022 },
+    { first: 'Ngọc',last: 'Đức',year: 2001,passed: 2022 },
+    { first: 'Ngọc',last: 'Đức',year: 2001,passed: 2022 },
+  ];
+
+  products: Product[] = [
+    {
+      productId: 1,
+      productName: 'Chổi',
+      productCode: '1234',
+      releaseDate: '12/12/2001',
+      price: 5000,
+      description: 'Mô tả',
+      starRating: 3,
+      imageUrl: "https://i.pinimg.com/originals/64/d5/f7/64d5f711af697325e65c5187b11290e9.jpg"
+    },
+    {
+      productId: 1,
+      productName: 'Chổi',
+      productCode: '1234',
+      releaseDate: '12/12/2001',
+      price: 5000,
+      description: 'Mô tả',
+      starRating: 3,
+      imageUrl: "https://i.pinimg.com/originals/64/d5/f7/64d5f711af697325e65c5187b11290e9.jpg"
+    },
+    {
+      productId: 1,
+      productName: 'Chổi',
+      productCode: '1234',
+      releaseDate: '12/12/2001',
+      price: 5000,
+      description: 'Mô tả',
+      starRating: 3,
+      imageUrl: "https://i.pinimg.com/originals/64/d5/f7/64d5f711af697325e65c5187b11290e9.jpg"
+    }
+  ];
 }
---
+++ src/app/data.interface.ts
@@ -0,0 +1,17 @@
+export interface Data {
+    first: string,
+    last: string,
+    year: number,
+    passed: number
+}
+
+export interface Product {
+    productId: number;
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    price: number;
+    description: string;
+    starRating: number;
+    imageUrl: string;   
+}
